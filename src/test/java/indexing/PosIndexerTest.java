f20f290ca340c848903f5406dcc627aea831e8a2
OttkO <adrian_iankov@abv.bg>
2017-01-08 17:30:54 +0100

    --Directories now work in Windows and Linux

--- src/test/java/indexing/PosIndexerTest.java
+++ src/test/java/indexing/PosIndexerTest.java
@@ -3,25 +3,28 @@ package indexing;
 
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * Created by OttkO on 06-Jan-17.
  */
 public class PosIndexerTest {
-
+    String tweetsDir = new File(new File(Config.pCloudRoot, "testAPIdata"), "tweets").getAbsolutePath();
+    String articleDir = new File(new File(Config.pCloudRoot, "testAPIdata"), "articles").getAbsolutePath();
     @Test
     public void testMakeArticleIndex() throws Exception {
         DbHandler.setupDatabase();
-        PosIndexer.makeArticleIndex(Config.articleDir);
+        PosIndexer.makeArticleIndex(articleDir);
     }
 
     @Test
     public void testMakeTwitterIndex() throws Exception {
         DbHandler.setupDatabase();
-        PosIndexer.makeTwitterIndex(Config.tweetsDir);
+        PosIndexer.makeTwitterIndex(tweetsDir);
     }
 
     @Test
     public void testBuildIndex() throws Exception{
-        PosIndexer.buildIndexes(Config.articleDir,Config.tweetsDir);
+        PosIndexer.buildIndexes(articleDir,tweetsDir);
     }
 }
