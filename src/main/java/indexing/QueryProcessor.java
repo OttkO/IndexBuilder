5be6f6952e388ccc1dafef47b17ec381a4c87c45
OttkO <adrian_iankov@abv.bg>
2017-01-12 19:37:59 +0100

    Sander's momma is fat as fuck

--- src/main/java/indexing/QueryProcessor.java
+++ src/main/java/indexing/QueryProcessor.java
@@ -53,16 +53,6 @@ public class QueryProcessor {
     }
     public static ArrayList<KeywordStructure> getTweetById(String id) throws SQLException {
         ArrayList<KeywordStructure> tweetIds = DbHandler.getKeywordPositionsTweetIds(id);
-
-        ArrayList<KeywordStructure> results = new ArrayList<>();
-        for (KeywordStructure k :
-                tweetIds) {
-            KeywordStructure found = DbHandler.findKeywordInfoTweetIndexes(k.fileName, id, 0, k.lineNumber); // look in the db
-            if (found != null) {
-                results.add(found);
-            }
-        }
-
-        return results;
+        return tweetIds;
     }
 }
